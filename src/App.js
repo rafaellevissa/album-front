252645ee9b0941a9ce79e64db2b8b87091263c60
rafaellevissa <rafaellevissa@gmail.com>
2021-05-16 20:27:59 -0300

    register screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './css/App.css';
 import Routes from './Routes';
 
 function App() {
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,12 +1,27 @@
-import react from 'react';
+import '../css/register.css';
+import {
+  TextField,
+  Button
+} from '@material-ui/core';
 
 function register() {
-    return (
+  return (
       <div>
-       <h1>Meus álbuns de fotos</h1>
-       <h3></h3>
-      </div>
-    );
-  }
+    <div className="App-header">
+     <h1 className="App-h1">Meus álbuns de fotos</h1>
+     <h3 className="App-h3">Faça o seu cadastro:</h3>
+     <form>
+     <TextField fullWidth className="App-name" name="name" type="text" id="outlined-basic" label="Nome" variant="outlined" />    
+     <TextField fullWidth className="App-email" name="email" type="email" id="outlined-basic" label="E-mail" variant="outlined" />
+     <TextField fullWidth className="App-password" name="password" type="password" id="outlined-basic" label="Senha" variant="outlined" />
+     <a className="App-link" href="/">Cancelar</a>
+     <Button className="App-entrar" variant="contained" color="primary">
+      Concluir
+     </Button>
+     </form>
+    </div>
+    </div>
+  );
+}
 
-  export default register;
+export default register;
