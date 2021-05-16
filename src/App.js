d3588e614844d2807b8563de6a77e36126e367d2
rafaellevissa <rafaellevissa@gmail.com>
2021-05-16 14:02:08 -0300

    initial react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Olá mundo!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
