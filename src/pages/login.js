dec3aab050ea089cf5a471c7f46239fd7d12ade3
rafaellevissa <rafaellevissa@gmail.com>
2021-05-19 15:23:46 -0300

    api integration

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,52 @@
 import '../css/App.css';
+import React, { useState } from 'react';
+import api from '../services/api';
 import {
   TextField,
   Button
 } from '@material-ui/core';
 
 function Login() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  async function auth() {
+    try{
+
+      const data = await api.post('/login', {
+        email: email,
+        password: password
+      })
+
+
+      localStorage.setItem('User',JSON.stringify(data))
+
+      console.log(data);  
+
+      return window.location.href = "/albuns"
+
+    }catch(error){
+      
+      alert("Senha ou Email são inválidos ");
+
+      console.log(error);
+    
+    }
+  
+  }
+
+  
   return (
       <div className="App">
     <div className="App-header">
      <h1 className="App-h1">Meus álbuns de fotos</h1>
      <h3 className="App-h3">Autentique-se</h3>
      <form>
-     <TextField required fullWidth className="App-email" name="email" type="email" id="outlined-basic" label="E-mail" variant="outlined" />
-     <TextField required fullWidth className="App-password" name="password" type="password" id="outlined-basic" label="Senha" variant="outlined" />
+     <TextField required fullWidth className="App-email" name="email" type="email" id="outlined-basic" label="E-mail" variant="outlined" onChange={ e => setEmail(e.target.value) } />
+     <TextField required fullWidth className="App-password" name="password" type="password" id="outlined-basic" label="Senha" variant="outlined" onChange={ e => setPassword(e.target.value) }/>
      <a className="App-link" href="/cadastro">Cadastre-se</a>
-     <Button className="App-entrar" variant="contained" color="primary">
+     <Button className="App-entrar" variant="contained" color="primary"  onClick={()=>auth()}>
       Entrar
      </Button>
      </form>
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,21 +1,51 @@
 import '../css/register.css';
+import api from '../services/api';
+import React, { useState } from 'react';
 import {
   TextField,
   Button
 } from '@material-ui/core';
 
-function register() {
+function Register() {
+  
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  console.log("pssa1");
+
+  async function cadastrar() {
+
+    try{
+      await api.post('/user',{
+      name:name,
+      email:email,
+      password:password,
+    })
+     
+     
+      alert("Voce foi cadastrado");
+
+      return window.location.href = "/albuns"
+
+    }
+    catch(error){ //DEBUG do ERRO
+      console.log('Houve erro!')
+    }
+  }
+
+  
   return (
       <div>
     <div className="App-header">
      <h1 className="App-h1">Meus álbuns de fotos</h1>
      <h3 className="App-h3">Faça o seu cadastro:</h3>
      <form>
-     <TextField required fullWidth className="App-name" name="name" type="text" id="outlined-basic" label="Nome" variant="outlined" />    
-     <TextField required fullWidth className="App-email" name="email" type="email" id="outlined-basic" label="E-mail" variant="outlined" />
-     <TextField required fullWidth className="App-password" name="password" type="password" id="outlined-basic" label="Senha" variant="outlined" />
+     <TextField required fullWidth className="App-name" name="name" type="text" id="outlined-basic" label="Nome" variant="outlined" onChange={ e => setName(e.target.value) }/>    
+     <TextField required fullWidth className="App-email" name="email" type="email" id="outlined-basic" label="E-mail" variant="outlined" onChange={ e => setEmail(e.target.value) }/>
+     <TextField required fullWidth className="App-password" name="password" type="password" id="outlined-basic" label="Senha" variant="outlined" onChange={ e => setPassword(e.target.value) }/>
      <a className="App-link" href="/">Cancelar</a>
-     <Button type="submit" href="/" className="App-entrar" variant="contained" color="primary">
+     <Button type="submit" className="App-entrar" variant="contained" color="primary" onClick={()=>cadastrar()}>
       Concluir
      </Button>
      </form>
@@ -24,4 +54,4 @@ function register() {
   );
 }
 
-export default register;
+export default Register;
